44823343f0badb7028334a214d87f0d034173415
matheusmatta21 <matheusalexandre2004@gmail.com>
2026-03-19 11:17:43 -0300

    fix: adjust background animation duration and positioning

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,8 @@
 export default function Home() {
   return (
     <div className="min-h-screen ">
-      <div className="bg-[url('/kisco.jpg')] bg-center min-h-screen flex items-center justify-center animate-slide-bg">
+      <div className="bg-[url('/kisco.jpg')]  min-h-screen flex items-center justify-center animate-slide-bg bg-left">
+      <div ></div>
         <h1 className="text-white text-3xl">KISCO</h1>
       </div>
     </div>
