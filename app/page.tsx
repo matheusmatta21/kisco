552524a70e404ef8e0e373e4551a005446576944
matheusmatta21 <matheusalexandre2004@gmail.com>
2026-03-19 22:53:11 -0300

    feat: update homepage layout and add CardSmall component

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,24 @@
+import { CardSmall } from "@/components/CardTest";
+
 export default function Home() {
   return (
     <div className="min-h-screen ">
-      <div className="bg-[url('/kisco.jpg')] bg-center min-h-screen flex items-center justify-center animate-slide-bg">
-        <h1 className="text-white text-3xl">KISCO</h1>
+      <div className="relative bg-[url('/kisco.jpg')] bg-center min-h-screen grid justify-center animate-slide-bg">
+        <div className="absolute inset-0 bg-linear-to-t from-black/70 via-black/10 to-transparent"></div>
+        <h1 className="relative z-10 text-white text-8xl font-bold text-center mt-8">
+          MALINHA PT.2
+        </h1>
+        <h1 className="relative z-10 text-white text-9xl font-bold text-center">
+          FALTAM 121 DIAS
+        </h1>
+      </div>
+      <div className="bg-gray-800 grid grid-cols-3 gap-6 pt-6">
+        <CardSmall />
+        <CardSmall />
+        <CardSmall />
+        <CardSmall />
+        <CardSmall />
+        <CardSmall />
       </div>
     </div>
   );
